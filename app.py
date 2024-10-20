5d5cc20376912a23356e16a31bdcd36039c9533b
Pavi Das <pavindas007@gmail.com>
2024-10-20 16:51:51 +0530

    Errors fixed

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
-from flask import Flask, request, render_template, redirect, session
+from flask import Flask, request,render_template, redirect,session
 from flask_sqlalchemy import SQLAlchemy
 import bcrypt
-
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
 app.secret_key = 'secret_key'
-
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -15,16 +13,13 @@ class User(db.Model):
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
 
-    def __init__(self, name, email, password):
+    def __init__(self,email,password,name):
         self.name = name
         self.email = email
-        self.password = bcrypt.hashpw(password.encode(
-            'utf-8'), bcrypt.gensalt()).decode('utf-8')
-        self.password = password
-
-    def check_password(self, password):
-        return bcrypt.checkpw(password.encode('utf-8'), self.password.encode('utf-8'))
-
+        self.password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+    
+    def check_password(self,password):
+        return bcrypt.checkpw(password.encode('utf-8'),self.password.encode('utf-8'))
 
 with app.app_context():
     db.create_all()
@@ -32,35 +27,39 @@ with app.app_context():
 
 @app.route('/')
 def index():
-    return render_template('home.html')
+    return render_template('index.html')
 
-
-@app.route('/register', methods=['get', 'post'])
+@app.route('/register',methods=['GET','POST'])
 def register():
-    if request.method == 'post':
-        # Handle requests
+    if request.method == 'POST':
+        # handle request
         name = request.form['name']
         email = request.form['email']
         password = request.form['password']
-        new_user = User(name=name, email=email, password=password)
+
+        new_user = User(name=name,email=email,password=password)
         db.session.add(new_user)
         db.session.commit()
-        return redirect('login')
+        return redirect('/login')
+
+
+
     return render_template('register.html')
 
-
-@app.route('/login', methods=['get', 'post'])
+@app.route('/login',methods=['GET','POST'])
 def login():
-    if request.method == 'post':
-        # Handle requests
+    if request.method == 'POST':
         email = request.form['email']
         password = request.form['password']
+
         user = User.query.filter_by(email=email).first()
+        
         if user and user.check_password(password):
             session['email'] = user.email
             return redirect('/dashboard')
         else:
-            return render_template('login.html', error='Invalid user')
+            return render_template('login.html',error='Invalid user')
+
     return render_template('login.html')
 
 
@@ -68,9 +67,14 @@ def login():
 def dashboard():
     if session['email']:
         user = User.query.filter_by(email=session['email']).first()
-        return render_template('dashboard.html', user=user)
+        return render_template('dashboard.html',user=user)
+    
     return redirect('/login')
 
+@app.route('/logout')
+def logout():
+    session.pop('email',None)
+    return redirect('/login')
 
 if __name__ == '__main__':
     app.run(debug=True)
